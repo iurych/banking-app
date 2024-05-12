c425b32f5b62283e49c90f4cfa958afb99f50069
iurych <iury.vibe@gmail.com>
2024-05-12 01:18:02 -0300

    feat: change global fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import Image from "next/image"
 
+//TODO criar a homepage
+// vídeo minutagem 19:57
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
