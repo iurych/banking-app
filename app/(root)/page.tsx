bcd65a098d862b26d08d61f3af3c5623f32f4327
iurych <iury.vibe@gmail.com>
2024-05-12 23:07:44 -0300

    chore: add HeaderBox and TotalBalanceBox components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 
 const Home = () => {
+  const loggedIn = { firstName: 'Iury'}
   return (
-    <div>Dashboard</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header" >
+         <HeaderBox
+          type='greeting'
+          title='welcome'
+          user={loggedIn?.firstName || 'Guest'}
+          subtext='Access and manage tour account and transactions efficently'
+        />
+        <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        />
+        </header>
+
+      </div>
+    </section>
   )
 }
 
