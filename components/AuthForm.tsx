bb6ebb5b8e1277ca5953209beccd13d54ffcde7b
iurych <iury.vibe@gmail.com>
2024-06-03 15:04:24 -0300

    feat: add Form and Input component

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -3,10 +3,48 @@ import Image from "next/image"
 import Link from "next/link"
 import { Fragment, useState } from "react"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters"
+  }).max(50),
+})
+
+type authForm = z.infer<typeof formSchema>
+
+
 const AuthForm = ({type}: {type:string}) => {
   const [user, setUser] = useState(null)
 
-  console.log(type)
+  // define a form
+  const form = useForm<authForm>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+    }
+  })
+
+  // define a submite handler
+  const onSubmit = async (values: authForm) => {
+    console.log(values)
+  }
+
   return (
     <section className="auth-form">
       <header className="flex flex-col gap-5 md:gap-8" >
@@ -46,7 +84,27 @@ const AuthForm = ({type}: {type:string}) => {
         </div>
       ): (
         <Fragment>
-          FORM
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
         </Fragment>
       )}
     </section>
